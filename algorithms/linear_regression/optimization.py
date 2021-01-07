41474c88411e7ced41a8593687e34219da18cfb0
Luke Grecki <lukegrecki@gmail.com>
2021-01-07 09:22:18 -0500

    Minor renaming for clarity

--- algorithms/linear_regression/optimization.py
+++ algorithms/linear_regression/optimization.py
@@ -16,7 +16,7 @@ class Hyperparameters:
 class Optimizer:
     def __init__(self, hyperparameters: Hyperparameters):
         self.hyperparameters = hyperparameters
-        self.solution = self.hyperparameters.initial_model
+        self.model = self.hyperparameters.initial_model
 
     def update(
         self,
@@ -28,10 +28,10 @@ class Optimizer:
         b_derivative = -2.0 * np.sum(data[:, 1] - predictions[:, 1])
         m_derivative = -2.0 * np.sum(data[:, 0] * (data[:, 1] - predictions[:, 1]))
 
-        b = self.solution.b - (b_derivative / n) * self.hyperparameters.learning_rate
-        m = self.solution.m - (m_derivative / n) * self.hyperparameters.learning_rate
+        b = self.model.b - (b_derivative / n) * self.hyperparameters.learning_rate
+        m = self.model.m - (m_derivative / n) * self.hyperparameters.learning_rate
 
-        self.solution = ModelParameters(m, b)
+        self.model = ModelParameters(m, b)
 
     def solve(self, data: np.ndarray) -> Optional[Solution]:
         raise NotImplementedError
@@ -43,14 +43,14 @@ class Optimizer:
 
     def evaluate(self):
         if self.loss < self.hyperparameters.tolerance:
-            return Solution(self.solution, self.loss)
+            return Solution(self.model, self.loss)
 
 
 class GradientDescent(Optimizer):
     def solve(self, data: np.ndarray) -> Optional[Solution]:
-        self.solution = self.hyperparameters.initial_model
+        self.model = self.hyperparameters.initial_model
         for epoch in range(self.hyperparameters.epochs):
-            predictions = predict(data, self.solution)
+            predictions = predict(data, self.model)
             self.loss = loss(data, predictions)
             self.log_progress(epoch)
 
@@ -65,17 +65,17 @@ class GradientDescent(Optimizer):
 
 class StochasticGradientDescent(Optimizer):
     def solve(self, data: np.ndarray) -> Optional[Solution]:
-        self.solution = self.hyperparameters.initial_model
+        self.model = self.hyperparameters.initial_model
         indices = np.arange(len(data))
         for epoch in range(self.hyperparameters.epochs):
             np.random.shuffle(indices)
 
             for index in indices:
                 point = data[index : index + 1]
-                predictions = predict(point, self.solution)
+                predictions = predict(point, self.model)
                 self.update(point, predictions)
 
-            self.loss = loss(data, predict(data, self.solution))
+            self.loss = loss(data, predict(data, self.model))
             self.log_progress(epoch)
 
             solution = self.evaluate()
