4380db1b3783c83997a6829c215fd758a6d6db38
Luke Grecki <lukegrecki@gmail.com>
2021-01-09 22:45:12 -0500

    Fix bug and add test case

--- algorithms/nearest_neighbors/core.py
+++ algorithms/nearest_neighbors/core.py
@@ -1,6 +1,5 @@
 from typing import Optional, List
 import numpy as np
-import math
 from dataclasses import dataclass
 from collections import Counter
 
@@ -20,7 +19,7 @@ def classify(
     data: np.ndarray, labels: List[str], point: np.ndarray, k: int
 ) -> Optional[str]:
     nearest_neighbors = []
-    max_distance = math.inf
+    max_distance = None
 
     for i, labeled_point in enumerate(data):
         d = distance(labeled_point, point)
@@ -29,13 +28,22 @@ def classify(
             nearest_neighbors.append(
                 Neighbor(point=labeled_point, distance=d, label=labels[i])
             )
-            max_distance = min(max_distance, d)
+
+            if not max_distance:
+                max_distance = d
+            else:
+                max_distance = max(max_distance, d)
         elif d < max_distance:
-            further_neighbor_indices = (
+            farthest_neighbor_indices = [
                 i for i, n in enumerate(nearest_neighbors) if n.distance == max_distance
+            ]
+            nearest_neighbors.pop(farthest_neighbor_indices.pop(0))
+            nearest_neighbors.append(
+                Neighbor(point=labeled_point, distance=d, label=labels[i])
             )
-            nearest_neighbors.pop(next(further_neighbor_indices))
-            nearest_neighbors.append(Neighbor(point=point, distance=d, label=labels[i]))
+
+            if not farthest_neighbor_indices:
+                max_distance = min(max_distance, d)
 
     nearest_labels = map(lambda n: n.label, nearest_neighbors)
     label_counts = Counter(nearest_labels)
--- tests/algorithms/nearest_neighbors/test_core_nearest_neighbors.py
+++ tests/algorithms/nearest_neighbors/test_core_nearest_neighbors.py
@@ -3,8 +3,14 @@ from algorithms.nearest_neighbors.core import classify
 
 
 def test_classify():
-    data = np.array([[0.0, 0.0], [2.0, 2.0], [4.0, 4.0]])
-    labels = ["cat", "cat", "dog"]
+    data = np.array([[0.0, 0.0], [4.0, 4.0], [2.0, 2.0]])
+    labels = ["cat", "dog", "cat"]
+
+    point = np.array([[3.5, 3.5]])
+    k = 1
+
+    assert classify(data, labels, point, k) == "dog"
+
     point = np.array([[1.0, 1.0]])
     k = 2
 
