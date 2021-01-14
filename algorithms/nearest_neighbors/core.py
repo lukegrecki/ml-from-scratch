4326df3c01d4959a744ec3f7f56438e8cce0015f
Luke Grecki <lukegrecki@gmail.com>
2021-01-14 13:26:23 -0500

    Add docstrings for nearest neighbors code

--- algorithms/nearest_neighbors/core.py
+++ algorithms/nearest_neighbors/core.py
@@ -6,18 +6,45 @@ from collections import Counter, defaultdict
 
 @dataclass
 class Neighbor:
+    """
+    A container class for nearest neighbors.
+
+    Attributes:
+        point: A numpy array representing a data point.
+        distance: Distance from this point to the point being considered.
+        index: Index of this data point in the original data array.
+    """
+
     point: np.ndarray
     distance: float
     index: int
 
 
 def distance(a: np.ndarray, b: np.ndarray) -> float:
+    """The standard l-2 norm of two numpy arrays.
+
+    Args:
+        a: A numpy array.
+        b: A numpy array.
+    """
+
     return np.linalg.norm(a - b)
 
 
 def find_nearest_neighbors(
     data: np.ndarray, point: np.ndarray, k: int
 ) -> List[Neighbor]:
+    """Find the k nearest neighbors given a data set and a given point.
+
+    Args:
+        data: A numpy array of data points.
+        point: The given point to find the neighbors of.
+        k: The number of nearest neighbors to find.
+
+    Returns:
+        A list of nearest neighbors.
+    """
+
     nearest_neighbors = []
     max_distance = None
 
@@ -47,6 +74,19 @@ def find_nearest_neighbors(
 def predict(
     data: np.ndarray, values: np.ndarray, point: np.ndarray, k: int
 ) -> Optional[float]:
+    """
+    Predict the value of the function at a given point.
+
+    Args:
+        data: A numpy array of data points.
+        values: The given values for each data point.
+        point: A numpy array of a point to predict the value of.
+        k: The number of nearest neighbors to find.
+
+    Returns:
+        The weighted prediction of k nearest neighbors if exists, None otherwise.
+    """
+
     nearest_neighbors = find_nearest_neighbors(data, point, k)
     weighted_values = [values[n.index] / n.distance for n in nearest_neighbors]
 
@@ -63,6 +103,19 @@ def classify(
     k: int,
     weighted: bool = False,
 ) -> Optional[str]:
+    """
+    Classify a point given a set of data points and labels.
+
+    Args:
+        data: A numpy array of data points.
+        labels: A list of labels corresponding to the data points.
+        point: The data point to classify.
+        k: The number of nearest neighbors to find.
+        weighted (optional): A boolean determining whether to weight the nearest classes by distance.
+
+    Returns:
+        The predicted class if it exists, None otherwise.
+    """
     nearest_neighbors = find_nearest_neighbors(data, point, k)
 
     if not weighted:
